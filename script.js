e86d2beed7f96f8256215f619d5844a0d6c4c09b
kcgr0690 <kcgr0690@gmail.com>
2025-09-29 19:19:16 -0400

    Copy pasted event listener, changed a few things since the submit button and teh enter button do the same exact thing.

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ addTaskButton.addEventListener("click", function() {
         const priority = prioritySelect.value;
         if (priority === "3") li.classList.add("high");
         else if (priority === "2") li.classList.add("medium");
-        else if (priority === 1)
+        else if (priority === "1") li.classList.add("low");
+        const checkButton = document.createElement("button");
+        checkButton.textContent = "✔";
+        li.appendChild(checkButton);
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+});
+
+taskInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter" && taskInput.value.trim() !== ""); {
+        const li = document.createElement("li");
+        const priority = prioritySelect.value;
+        if (priority === "3") li.classList.add("high");
+        else if (priority === "2") li.classList.add("medium");
+        else if (priority === "1") li.classList.add("low");
+        const checkButton = document.createElement("button");
+        checkButton.textContent = "✔";
+        li.appendChild(checkButton);
+        taskList.appendChild(li);
+        taskInput.value = "";
     }
 });
