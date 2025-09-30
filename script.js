c56123e9e5eca081aefd42e53706d843e9348413
kcgr0690 <kcgr0690@gmail.com>
2025-09-29 20:43:32 -0400

    edited js, edted css

--- script.js
+++ script.js
@@ -4,22 +4,25 @@ const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
 
 addTaskButton.addEventListener("click", function() {
-    if (taskInput.value.trim() !== ""); {
+    if (taskInput.value.trim() !== "") {
         const li = document.createElement("li");
         const priority = prioritySelect.value;
+        const taskText = document.createElement("span");
+        taskText.textContent = taskInput.value;
         if (priority === "3") li.classList.add("high");
         else if (priority === "2") li.classList.add("medium");
         else if (priority === "1") li.classList.add("low");
+        li.appendChild(taskText);
         const checkButton = document.createElement("button");
         checkButton.textContent = "✔";
-        li.appendChild(checkButton);
+        li.appendChild(checkButton)
         taskList.appendChild(li);
         taskInput.value = "";
     }
 });
 
 taskInput.addEventListener("keypress", function(event) {
-    if (event.key === "Enter" && taskInput.value.trim() !== ""); {
+    if (event.key === "Enter" && taskInput.value.trim() !== "") {
         const li = document.createElement("li");
         const priority = prioritySelect.value;
         if (priority === "3") li.classList.add("high");
